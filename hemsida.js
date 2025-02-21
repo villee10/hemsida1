28f5638d1050ab1f89324ffe37414be5b3bc4273
William Eliasson <ville@MacBook-Pro-som-tillhor-William.local>
2025-02-21 20:44:12 +0100

    rm

--- hemsida.js
+++ hemsida.js
@@ -1,7 +1,1 @@
 
-
-document.addEventListener('DOMContentLoaded', function(){
-    const loginContainer = document.querySelector('.skapakontocontainer');
-    const registerContainer = document.querySelector('.registreracontainer');
-    const showRegister = document.querySelector('.registrera');
-}
